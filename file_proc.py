22412441638585a99cf28c438162b3e76819b4d0
Māris Danne <Maris.Danne@rvt.lv>
2020-02-03 13:34:23 +0200

    add read_file

--- file_proc.py
+++ file_proc.py
@@ -1,7 +1,7 @@
 # a - Will append to the end of the file
 # w - Will overwrite any existing content
 
-file_name = 'file.txt'
+file_name = 'data.txt'
 
   # with open(file_name, 'w') as f:       
   #     f.write(line)
